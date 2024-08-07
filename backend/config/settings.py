e4bd959af4170de8767696402eaadff2a552e746
divanster <divanster@gmail.com>
2024-08-07 16:37:32 +0300

    tests

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -97,6 +97,9 @@ DATABASES = {
         'PASSWORD': os.environ.get('DB_PASSWORD'),
         'HOST': os.environ.get('DB_HOST'),
         'PORT': os.environ.get('DB_PORT', '5432'),
+        'TEST': {
+            'NAME': os.environ.get('TEST_DB_NAME', 'test_' + os.environ.get('DB_NAME')),
+        },
     }
 }
 
