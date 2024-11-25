ce8b5d8dd535400cbf0d14ea115e9f100a2a5039
divanster <divanster@gmail.com>
2024-11-25 17:55:23 +0200

    fixing graph

--- backend/schema.py
+++ backend/schema.py
@@ -1,4 +1,8 @@
 import graphene
+from graphene_django.types import DjangoObjectType
+from django.contrib.auth import get_user_model
+
+# Your other schema imports
 import albums.schema
 import users.schema
 import stories.schema
@@ -12,6 +16,24 @@ import newsfeed.schema
 import social.schema
 import messenger.schema
 
+# Get the custom User model
+User = get_user_model()
+
+# Define the User GraphQL type
+class UserType(DjangoObjectType):
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'email', 'first_name', 'last_name')  # Add other fields if needed
+
+# Define the Query for 'me'
+class MeQuery(graphene.ObjectType):
+    me = graphene.Field(UserType)
+
+    def resolve_me(self, info):
+        user = info.context.user
+        if user.is_anonymous:
+            raise Exception("Authentication required to view this information.")
+        return user
 
 # Combine Queries from all the different apps
 class Query(
@@ -27,10 +49,10 @@ class Query(
     newsfeed.schema.Query,
     social.schema.Query,
     messenger.schema.Query,
+    MeQuery,  # Add the MeQuery here to include the 'me' field
     graphene.ObjectType,
 ):
     pass
-
 
 # Combine Mutations from all the different apps
 class Mutation(
@@ -49,7 +71,6 @@ class Mutation(
     graphene.ObjectType,
 ):
     pass
-
 
 # Define the schema
 schema = graphene.Schema(query=Query, mutation=Mutation)
